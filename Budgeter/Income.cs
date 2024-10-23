99d0be558867af08ce0def1d90037174eaf6d79e
Joe Namihas <josephnamihas@yahoo.co.uk>
2024-10-23 15:06:05 +0100

    Add project files.

---
+++ Budgeter/Income.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budgeter
+{
+    public class Income
+    {
+        public string Name { get; set; }
+        public int IncomeAmount { get; set; }
+
+        public Income(string _name, int _amount)
+        {
+            Name = _name;
+            IncomeAmount = _amount;
+        }
+
+        // Function for Total Amount
+
+
+    }
+}
---
+++ Budgeter/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Budgeter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<Income> IncomingFunds = new List<Income>()
+        {
+            
+
+        };
+        
+
+      
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //ListBoxIncome.ItemsSource = IncomingFunds;
+        }
+
+        private void IncomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            int userVal = int.Parse(IncomeAmount.Text);
+            ListBoxIncome.Items.Add(new Income(IncomeName.Text, userVal));
+          
+        }
+
+        
+    }
+}
